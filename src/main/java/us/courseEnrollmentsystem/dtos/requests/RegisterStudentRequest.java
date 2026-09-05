30cf2ad6f0dc896ec2a07f92f7b0903978833814
A_Abdulazeez <adebex999@gmail.com>
2026-09-05 02:01:09 +0100

    renamed from CreateStudentRequest and CreateStudentResponse to RegisterStudentRequest and RegisterStudentResponse

--- src/main/java/us/courseEnrollmentsystem/dtos/requests/RegisterStudentRequest.java
+++ src/main/java/us/courseEnrollmentsystem/dtos/requests/RegisterStudentRequest.java
@@ -3,8 +3,9 @@ package us.courseEnrollmentsystem.dtos.requests;
 import lombok.Data;
 
 @Data
-public class CreateStudentRequest {
+public class RegisterStudentRequest {
     private String name;
     private String email;
+    private String password;
     private String department;
 }
