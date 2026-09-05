cc05285cb3507a47b856a158ad778d12917d83d7
A_Abdulazeez <adebex999@gmail.com>
2026-09-05 02:35:23 +0100

    added password to the test and test for password

--- src/test/java/us/courseEnrollmentsystem/services/AuthServiceImplTest.java
+++ src/test/java/us/courseEnrollmentsystem/services/AuthServiceImplTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.NullSource;
+import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import us.courseEnrollmentsystem.data.repositories.StudentRepository;
@@ -51,11 +52,25 @@ public class AuthServiceImplTest {
         assertThrows(StudentException.class, () -> authService.registerStudent(createStudentRequest));
     }
 
+    @ParameterizedTest
+    @ValueSource(strings = {"1","12", "123", "1234", "12345"})
+    public void registerStudentWithInvalidPasswordThrowsStudentExceptionTest(String password){
+        RegisterStudentRequest createStudentRequest = new RegisterStudentRequest();
+        createStudentRequest.setName("Azeez");
+        createStudentRequest.setEmail("az@gmail.com");
+        createStudentRequest.setDepartment("Biochemistry");
+        createStudentRequest.setPassword(password);
+
+        assertThrows(StudentException.class, ()-> authService.registerStudent(createStudentRequest));
+
+    }
+
     @Test
     public void registerStudentWithValidRequestRepositoryCountIsOneTest(){
         createStudentRequest.setName("Azeez");
         createStudentRequest.setEmail("az@gmail.com");
         createStudentRequest.setDepartment("Biochemistry");
+        createStudentRequest.setPassword("123456");
 
         authService.registerStudent(createStudentRequest);
         assertEquals(1, studentRepository.count());
@@ -66,12 +81,14 @@ public class AuthServiceImplTest {
         createStudentRequest.setName("Azeez");
         createStudentRequest.setEmail("az@gmail.com");
         createStudentRequest.setDepartment("Biochemistry");
+        createStudentRequest.setPassword("123456");
         authService.registerStudent(createStudentRequest);
 
         RegisterStudentRequest createStudentRequest2 = new RegisterStudentRequest();
         createStudentRequest2.setName("Bola");
         createStudentRequest2.setEmail("az@gmail.com");
         createStudentRequest2.setDepartment("chemistry");
+        createStudentRequest2.setPassword("123456");
         assertThrows(StudentException.class, () -> authService.registerStudent(createStudentRequest2));
 
     }
